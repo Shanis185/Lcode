1378f330f67e6135a40b6b9746537f74870356ac
Shanis185 <shaniswrk2003@gmail.com>
2025-06-08 23:40:52 +0530

    .

--- leetcode152.py
+++ leetcode152.py
@@ -1,14 +1,12 @@
 class Solution(object):
     def maxProduct(self, nums):
-        if not nums:
-            return 0
-        if len(nums)==1:
-            return nums[0]
-        target=float('-inf')
-        total=nums[0]
-        for i in range(len(nums)):
-            
-            for j in range(i+1,len(nums)):
-                total*=nums[j]
-                target=max(target,total)
-        return target
+        curr_max=curr_min=result=nums[0]
+
+        for i in nums[1:]:
+            if i<0:
+                curr_max,curr_min=curr_min,curr_max
+            curr_max=max(i,curr_max*i)
+            curr_min=min(i,curr_min*i)
+            result=max(result,curr_max)
+        return result
+        
