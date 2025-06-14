333aaf31fb72c049ae32d96945c55ee2f8616106
Shanis185 <shaniswrk2003@gmail.com>
2025-06-14 22:44:58 +0530

    .

---
+++ leetcode1614.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxDepth(self, s: str) -> int:
+        max_depth=0
+        curr_depth=0
+        for i in s:
+            if i=="(":
+                curr_depth+=1
+                max_depth=max(curr_depth,max_depth)
+            elif i==")":
+                curr_depth-=1
+        return max_depth
