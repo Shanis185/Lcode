0bc53e62f039e1a42f7f07f69980b30723365300
Shanis185 <shaniswrk2003@gmail.com>
2025-07-07 00:23:15 +0530

    .

---
+++ leetcode104.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        q=deque()
+        if not root:
+            return 0
+        q.append(root)
+        res=[]
+        level=[]
+        while q:
+            n=len(q)
+            for i in range(n):
+                node=q.popleft()
+                level.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            res.append(level)
+        return len(res)
